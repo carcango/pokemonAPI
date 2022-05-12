2f143ac8bed75ba4ce0aa166fa1ceb493c4c6fb1
Carson.O <carcango@gmail.com>
2022-05-12 11:57:20 -0700

    added local JSON / created req/res function

--- server.js
+++ server.js
@@ -1,64 +1,43 @@
 //-----------------------------------------Server Setup-------------------------------------//
 const express = require("express");
-var cors = require('cors')
+var cors = require("cors");
 const app = express();
 app.set("view engine", "ejs");
 const https = require("https");
 const mongoose = require("mongoose");
 const bodyparser = require("body-parser");
+const pokemonFile = require("./first_30_pokemon.json");
 
 app.listen(process.env.PORT || 5001, function (err) {
   if (err) console.log(err);
 });
 
-app.use(cors())
+app.use(cors());
 
 app.use(
   bodyparser.urlencoded({
     extended: true,
   })
 );
-//----------------------------------Database initialization ----------------------------------//
-mongoose.connect(
-  "mongodb+srv://carcango:2537@cluster0.tcuqv.mongodb.net/WebDev-2537?retryWrites=true&w=majority",
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  }
-);
 
-const pokemonSchema = new mongoose.Schema({
-  abilities: Array,
-  base_experience: Number,
-  forms: Array,
-  game_indices: Array,
-  height: Number,
-  held_items: Array,
-  id: Number,
-  is_default: Boolean,
-  location_area_encounters: String,
-  moves: Array,
-  name: String,
-  order: Number,
-  species: {},
-  sprites: {},
-  stats: Array,
-  types: Array,
-  weight: Number,
-});
-const pokemonModel = mongoose.model("pokemon", pokemonSchema);
+app.get("/:id", function (req, res) {
+  console.log("looking for pokemon");
+  console.log(req.params.id);
+  console.log(typeof req.params.id);
+  current_poke = req.params.id
+  current_poke = parseInt(current_poke)
+  console.log(typeof current_poke);
 
-//------------------------------------Request handling ------------------------------------//
 
-app.get('/', function (req, res) {
-  pokemonModel.find({ $or: [{name: req.body, id: req.body}]}, function (err, poke_data) {
-      if (err) {
-          console.log("Error " + err);
-      } else {
 
-          console.log("Data " + poke_data);
-          console.log(poke_data)
-      }
+  for (i in pokemonFile) {
+
+    if (req.params.id == pokemonFile[i]["id"] || req.params.id == pokemonFile[i]["name"]){
+      console.log("pokemon found")
+      poke_data = pokemonFile[i]
+      console.log(poke_data)
       res.send(poke_data);
-  });
-})
+    }
+
+  }
+});
